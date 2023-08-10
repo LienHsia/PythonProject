aa12c57090a05ba9684a58dca91b0135ca3e3202
JiuCi <LiHsiang.Duan@gmail.com>
2023-08-10 22:26:51 +0800

    Create TestDemo4.py

---
+++ pythonDemo/TestDemo4.py
@@ -0,0 +1,38 @@
+
+from functools import reduce
+
+CHAR_TO_FLOAT = {
+    '0': 0,
+    '1': 1,
+    '2': 2,
+    '3': 3,
+    '4': 4,
+    '5': 5,
+    '6': 6,
+    '7': 7,
+    '8': 8,
+    '9': 9,
+    '.': -1
+}
+
+
+def str2float(s):
+    point = 0
+
+    def char2num(s1):
+        return CHAR_TO_FLOAT[s1]
+
+    def to_float(f, n):
+        nonlocal point
+        if n == -1:
+            point = 1
+            return f
+        if point == 0:
+            return f * 10 + n
+        else:
+            point = point * 10
+            return f + n / point
+    return reduce(to_float, map(char2num, s), 0.0)
+
+
+print(str2float('1344323.456'))
