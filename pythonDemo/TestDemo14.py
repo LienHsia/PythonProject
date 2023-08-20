cc456da386726e05a32a889eabe1f05ce1e049c3
JiuCi <LiHsiang.Duan@gmail.com>
2023-08-20 21:55:59 +0800

    Create TestDemo14.py

---
+++ pythonDemo/TestDemo14.py
@@ -0,0 +1,20 @@
+按名字排序：
+L = [('Bob', ), ('Adam', ), ('Bart', ), ('Lisa', )]
+
+
+def by_name(n):
+    return n[].lower()
+
+
+print('sorted by name: ')
+print(sorted(L, key=by_name))
+那分数排序：
+L = [('Bob', ), ('Adam', ), ('Bart', ), ('Lisa', )]
+
+
+def by_score(n):
+    return n[]
+
+
+print('sorted by score: ')
+print(sorted(L, key=by_score))
